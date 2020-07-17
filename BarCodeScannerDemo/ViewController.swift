8a6e8b24897603276a9aa03f602c135af7ed29f5
Ethan <ethan1022@gmail.com>
2020-07-18 01:02:46 +0800

    Initial Commit

---
+++ BarCodeScannerDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BarCodeScannerDemo
+//
+//  Created by Ethan on 2020/7/18.
+//  Copyright © 2020 Ethans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
